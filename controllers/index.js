83fa8c11d6d7c50723e21f1cb27b4fe3583706fe
swoozeki <sujitkarki95@gmail.com>
2018-03-14 19:46:33 -0400

    initialized controllers for login, logout, and register POST

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,16 @@
 const express = require("express");
 const router = express.Router();
 
+const UserController = require('./user-controller.js')
+
 router.use(express.static('./public'));
 
 router.get("/", (req, res) => {
   res.sendFile('index.html', {root: './'});
 });
+
+router.post("/register", UserController.user_register_post);
+router.post("/login", UserController.user_login_post);
+router.post("/logout", UserController.user_logout_post);
 
 module.exports = router;
---
+++ controllers/user-controller.js
@@ -0,0 +1,11 @@
+module.exports.user_register_post = function(req, res, next){
+    res.send('This is the login_post page');
+};
+
+module.exports.user_login_post = function(req, res, next){
+    res.send('This is the login_post page');
+};
+
+module.exports.user_logout_post = function(req, res, next){
+    res.send('This is the logout_post page');
+};
