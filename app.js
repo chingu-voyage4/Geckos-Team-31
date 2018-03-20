11fadd39a657be7c3614e5aa5d10a0051097d44c
swoozeki <sujitkarki95@gmail.com>
2018-03-20 10:48:29 -0400

    setup and connected to database using sequelize orm for postgres

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Sequelize = require('sequelize'); 
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -6,6 +7,18 @@ const app = express();
     process.env will be populated from the .env file
 */
 require('dotenv').config();
+
+/*
+    initialize and connect to postgres database using sequelize ORM
+*/
+const {PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE} = process.env;
+const sequelize = new Sequelize(
+    `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`
+);
+
+sequelize.authenticate()
+.then(()=>console.log('Database connection has been established successfully!'))
+.catch((err)=>console.error(err));
 
 /*
     use the bodyParser middleware to populate request body as an object in Request.body
@@ -18,6 +31,11 @@ app.use(jsonParser, urlencodedParser);
     use router in controllers folder for all routing
 */
 app.use("/", require("./controllers/routes.js"));
+
+/*
+    for any unconfigured routes
+*/
+app.get('*', (req, res, next)=>res.send('Here lies nothing!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started. App listening on ${PORT}`));
